fe986805c7b11124fac75d9d873005d17403eeef
vaibhavjadhav <vaibhav.jadhav0596@gmail.com>
2021-04-16 23:39:37 +0530

    Footer link s=done

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -11,27 +11,27 @@ function TheFooter() {
         <div className="footer-link-wrapper">
           <div class="footer-link-items">
             <h2>About Us</h2>
-            <Link to="/gallery">Our Origin</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Terms of Service</Link>
+            <Link to="/about">Our Origin</Link>
+            <Link to="/gallery">Gallery</Link>
+            <Link to="/contact">Careers</Link>
+            <Link to="/about">Terms of Service</Link>
           </div>
           <div class="footer-link-items">
             <h2>Contact Us</h2>
-            <Link to="/">Contact</Link>
-            <Link to="/">Support</Link>
-            <Link to="/">Destinations</Link>
-            <Link to="/">Sponsorships</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/contact">Support</Link>
+            <Link to="/courses">Courses</Link>
+            <Link to="/contact">Sponsorships</Link>
           </div>
         </div>
         <div className="footer-link-wrapper">
          
           <div class="footer-link-items">
             <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Youtube</Link>
-            <Link to="/">Twitter</Link>
+            <a href="https://www.instagram.com/narendramodi/" target="_blank">Instagram</a>
+            <a href="https://www.facebook.com/SMVITAPGDAC" target="_blank">Facebook</a>
+            <a href="https://www.youtube.com/channel/UCYlE74jGPXrd6jxlWpvjX_g" target="_blank">Youtube</a>
+            <a href="https://twitter.com/narendramodi?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank">Twitter</a>
           </div>
         </div>
         <div className="footer-link-wrapper">
